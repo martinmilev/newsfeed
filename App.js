48807003fd08231ab7fc37d0dbb1b621e725c551
Martin Milev <martinmariusmilev@gmail.com>
2021-03-11 22:54:27 +0200

    feat: dinamic search added

--- App.js
+++ App.js
@@ -1,40 +1,69 @@
-import React from 'react'
-import { useState, useEffect } from "react";
-import { ScrollView } from 'react-native'
-import { Content, Card, CardItem, Text, Thumbnail } from 'native-base'
+import React, { Fragment } from 'react'
+import { useState, useEffect } from 'react'
+import { FlatList } from 'react-native'
+import {
+  Content,
+  Card,
+  CardItem,
+  Text,
+  Thumbnail,
+  Header,
+  Item,
+  Input
+} from 'native-base'
 
 const App: () => React$Node = () => {
-  const uri = 'https://facebook.github.io/react-native/docs/assets/favicon.png'
-
-  var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=bg&' +
-          'apiKey=2ba583d7861040c4990efa916a04351d';
-  var req = new Request(url);
+  const apiUrl = 'https://newsapi.org/v2'
+  const apiKey = '4904a297062d46c59e955ebd0e7db5af'
 
   const [state, setState] = useState([])
 
-  const fetchArtices = async () => {
-    let result = await fetch(url).then(response => response.json())
-    console.log('result.articles', result.articles)
+  const fetchArtices = async (query) => {
+    let url =
+      query === ''
+        ? `${apiUrl}/top-headlines?country=us&apiKey=${apiKey}`
+        : `${apiUrl}/everything?q=${query}&apiKey=${apiKey}`
+
+    let result = await fetch(url).then((response) => response.json())
+
+    setState([])
     setState(result.articles)
   }
 
-    useEffect(()  => { fetchArtices() }, [])
+  useEffect(() => {
+    fetchArtices('')
+  }, [])
 
   return (
-    <>
-      <ScrollView>
-        <Content>
-          <Card>
-            <CardItem>
-              <Thumbnail square source={{ uri }} />
-              <Text>News Headline 1</Text>
-            </CardItem>
-          </Card>
-        </Content>
-      </ScrollView>
-    </>
+    <Fragment>
+      <Header searchBar rounded>
+        <Item>
+          <Input
+            placeholder='Search'
+            onChangeText={(text) => fetchArtices(text)}
+          />
+        </Item>
+      </Header>
+      <FlatList
+        data={state}
+        renderItem={ArticleCard}
+        keyExtractor={(item) => `${item.title}`}
+        onRefresh={() => fetchArtices()}
+        refreshing={false}
+      />
+    </Fragment>
   )
 }
 
 export default App
+
+const ArticleCard = ({ item }) => (
+  <Content>
+    <Card>
+      <CardItem>
+        <Thumbnail square source={{ uri: item.urlToImage }} />
+        <Text>{item.title}</Text>
+      </CardItem>
+    </Card>
+  </Content>
+)
